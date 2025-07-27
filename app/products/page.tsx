6d1eb6ce925893b93569e8ae44adca23716ab1d4
KaenNietsch <kaennietsch355@gmail.com>
2025-07-27 19:03:36 +0300

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,6 +6,14 @@ import ProductCatalog from './ProductCatalog';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
+//EK
+import dynamic from 'next/dynamic';
+
+const ProductCategories = dynamic(() => import('./ProductCategories'), { ssr: false });
+const ProductsHero = dynamic(() => import('./ProductsHero'), { ssr: false });
+const ProductCatalog = dynamic(() => import('./ProductCatalog'), { ssr: false });
+//EK
+
 export default function ProductsPage() {
   return (
     <div className="bg-black text-white overflow-hidden">
